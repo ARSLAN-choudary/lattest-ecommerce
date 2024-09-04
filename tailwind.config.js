f15cc7f9e53b5ae6d67df4f2574094eccfc56045
Arslan <arslanchoudary23@gmail.com>
2024-09-04 17:23:04 +0500

    all done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class', 
+  darkMode: 'class',
   content: [
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-dark': 'linear-gradient(90deg,_rgba(19,_1,_34,_1)_1%,_rgba(30,_1,_51,_1)_51%,_rgba(20,_1,_26,_1)_100%)',
+      },
+    },
   },
   plugins: [],
 }
